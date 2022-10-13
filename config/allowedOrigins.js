f86723417d06abfc1e88ad359056b4510fcf2a30
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-10-14 00:34:17 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://linkpeti-by-dojeto.vercel.app"
+    "https://linkpeti-by-dojeto.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
   ];
