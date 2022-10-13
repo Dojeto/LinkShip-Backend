699998a4b27005548eaf4858b87bc5839f88e72d
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-10-14 00:29:13 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,8 @@
 const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://ssip-2022.vercel.app",
-    "https://radiant-frangollo-31134a.netlify.app",
-     "http://localhost:3000",
+    "https://linkpeti-by-dojeto.app"
+    "http://localhost:3000",
     "http://127.0.0.1:3000"
   ];
   export default allowedOrigins;
