a03d27dfbb5b6d8e9c5dc847aafc72e661ff0455
Dojeto <omlimdiwala25@gmail.com>
2022-10-15 21:39:46 +0530

    Project Name Changed

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/',(req,resp)=>{
-    resp.send("Welcome to the LinkPeti backend");
+    resp.send("Welcome to the LinkShip backend");
 })
 
 export default router
